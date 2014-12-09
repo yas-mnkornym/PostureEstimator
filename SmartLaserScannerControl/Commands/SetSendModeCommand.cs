5656ddee421c8515ff5a7a135965c99d263e9fb9
yas-mnkornym <yasuhiro@aurora.ocn.ne.jp>
2014-12-09 11:55:47 +0900

    SendModeの変更コマンド実�

--- SmartLaserScannerControl/Commands/SetSendModeCommand.cs
+++ SmartLaserScannerControl/Commands/SetSendModeCommand.cs
@@ -6,8 +6,63 @@ using System.Threading.Tasks;
 
 namespace SmartLaserScannerControl.Commands
 {
-	public class SetSendModeCommand
+	public class SetSendModeCommand : ICommand
 	{
+		ESendMode sendMode_ = ESendMode.None;
+		bool value_ = false;
+
+		public SetSendModeCommand(ESendMode sendMode, bool value)
+		{
+			sendMode_ = sendMode;
+			value_ = value;
+		}
+
+		public CommandData GetData()
+		{
+			return new CommandData {
+				Address = SendModeToString(sendMode_),
+				Arguments = new object[]{
+					value_ ? 1 : 0
+				}
+			};
+		}
+
+		string SendModeToString(ESendMode sendMode)
+		{
+			switch (sendMode) {
+				case ESendMode.Osc:
+					return "sendOSC";
+
+				case ESendMode.Area:
+					return "sendArea";
+
+				case ESendMode.Pos:
+					return "sendPos";
+
+				case ESendMode.AnchorPos:
+					return "sendAnchorPos";
+
+				case ESendMode.Regions:
+					return "sendRegions";
+
+				case ESendMode.Touched:
+					return "sendTouched";
+
+				default:
+					return "";
+			}
+		}
+	}
+
+	public enum ESendMode
+	{
+		None,
+		Osc,
+		Area,
+		Pos,
+		AnchorPos,
+		Regions,
+		Touched,
 	}
 
 }
